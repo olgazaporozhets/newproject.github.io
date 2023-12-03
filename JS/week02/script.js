71d2fbe60252ca60143786e21c182560b5979535
olgazaporozhets <148690648+olgazaporozhets@users.noreply.github.com>
2023-12-03 01:39:05 +0100

    Update script.js

--- JS/week02/script.js
+++ JS/week02/script.js
@@ -49,31 +49,31 @@ let lengthOfString = myString.length;
 console.log("The length of myString is:", lengthOfString);
 
 //Task8
-// 8.1 Declare at least four variables with different data types.
+// 8.1 I ceclare at least four variables with different data types.
 let variable1 = 3;            // Number
 let variable2 = "Hello";      // String
 let variable3 = true;         // Boolean
 let variable4 = [1, 2, 3];     // Array
 
-// 8.2 For each variable, write a console.log statement that logs the value.
+// 8.2 For each variable, I write a console.log statement that logs the value.
 console.log("The value of variable1 is: " + variable1);
 console.log("The value of variable2 is: " + variable2);
 console.log("The value of variable3 is: " + variable3);
 console.log("The value of variable4 is: " + JSON.stringify(variable4));
 
-// 8.3 Explain in words the expected types of the variables.
+// 8.3 I explain in words the expected types of the variables.
 console.log("Variable1 is expected to be a Number.");
 console.log("Variable2 is expected to be a String.");
 console.log("Variable3 is expected to be a Boolean.");
 console.log("Variable4 is expected to be an Array.");
 
-// 8.4 Use typeof to log the actual type of the variables.
+// 8.4 I use typeof to log the actual type of the variables.
 console.log("The type of variable1 is: " + typeof variable1);
 console.log("The type of variable2 is: " + typeof variable2);
 console.log("The type of variable3 is: " + typeof variable3);
 console.log("The type of variable4 is: " + typeof variable4);
 
-// 8.5 Compare the types of different variables and log the result.
+// 8.5 I compare the types of different variables and log the result.
 if (typeof variable1 === typeof variable2 &&
     typeof variable2 === typeof variable3 &&
     typeof variable3 === typeof variable4) {
@@ -83,39 +83,39 @@ if (typeof variable1 === typeof variable2 &&
 }
 
 //Task9
-// Assuming x is declared elsewhere in your code, you don't need to use 'let' again.
-// Remove the 'let' keyword to avoid the 'Identifier 'x' has already been declared' error.
+// As x is declared elsewhere in my code, I don't need to use 'let' again.
+// I remove the 'let' keyword to avoid the 'Identifier 'x' has already been declared' error.
 
 // Given value of x
 x = 7;
 
-// Show the initial value of x
+// I show the initial value of x
 console.log("Initial value of x: " + x);
 
-// Explain what the % (modulo) operator does
+// I explain what the % (modulo) operator does
 console.log("The % (modulo) operator gives the remainder when x is divided by 3.");
 
-// Show the result of x % 3 without assigning it back to x
+// I show the result of x % 3 without assigning it back to x
 console.log("Result of x % 3 without assignment: " + (x % 3));
 
-// Update the value of x using x % 3
+// I update the value of x using x % 3
 x = x % 3;
 
-// Show the new value of x after the operation x = x % 3
+// I show the new value of x after the operation x = x % 3
 console.log("New value of x after x = x % 3: " + x);
 
 //Task10
-// 10.1 Can you store multiple types in an array? Numbers and strings? Make an example that illustrates your answer.
+// 10.1 I can store multiple types in an array, both numbers and strings. Here is an example that illustrates this.
 let mixedArray = [42, "Hello", true, { key: "value" }];
 
-// Log the array to show that it contains multiple types
+// I log the array to show that it contains multiple types
 console.log("Array with multiple types:", mixedArray);
 
-// 10.2 Can you compare infinities? (Not in Eyad's world) - does 6/0 === 10/0? How can you test this?
+// 10.2 I compare infinities: (Not in Eyad's world) - 6/0 === 10/0
 let result1 = 6 / 0;
 let result2 = 10 / 0;
 
-// Log the results and compare them
+// I log the results and compare them
 console.log("Result of 6/0:", result1);
 console.log("Result of 10/0:", result2);
 
